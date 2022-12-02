788fe6aa8b7738a5c126f572232c4811c16fdb8d
Alberto Restifo <alberto@restifo.dev>
2022-12-02 12:44:15 +0100

    Day 2 part 2 solution

--- day-02/main.go
+++ day-02/main.go
@@ -8,7 +8,33 @@ import (
 	"strings"
 )
 
+const (
+	_ int = iota
+	Rock
+	Paper
+	Scissors
+)
+
+const (
+	LostScore    = 0
+	DrawScore    = 3
+	WinningScore = 6
+)
+
+var winMatrix = map[int]int{
+	Rock:     Scissors,
+	Paper:    Rock,
+	Scissors: Paper,
+}
+
+var lostMatrix = make(map[int]int)
+
 func main() {
+	// Invert the win matrix into the lost matrix
+	for key, value := range winMatrix {
+		lostMatrix[value] = key
+	}
+
 	file, err := os.Open("./day-02/input.txt")
 	if err != nil {
 		log.Fatal(err)
@@ -36,59 +62,42 @@ func main() {
 	log.Printf("The total score is %d", totalScore)
 }
 
-const (
-	_ int = iota
-	Rock
-	Paper
-	Scissors
-)
-
-const (
-	LostScore    = 0
-	DrawScore    = 3
-	WinningScore = 6
-)
-
-var winMatrix = map[int]int{
-	Rock:     Scissors,
-	Paper:    Rock,
-	Scissors: Paper,
-}
-
 func calculateLineScore(line string) (int, error) {
 	elements := strings.Split(line, " ")
 	if len(elements) != 2 {
 		return 0, fmt.Errorf("line does not have 2 elements: %s", line)
 	}
 
-	them, err := mapToConst(elements[0])
+	them, err := mapToElement(elements[0])
 	if err != nil {
 		return 0, fmt.Errorf("reading them: %v", err)
 	}
 
-	us, err := mapToConst(elements[1])
-	if err != nil {
-		return 0, fmt.Errorf("reading us: %v", err)
-	}
+	switch elements[1] {
+	case "X":
+		// We want to loose
+		return winMatrix[them], nil
 
-	if us == them {
-		return DrawScore + us, nil
-	}
+	case "Y":
+		// Draw
+		return DrawScore + them, nil
 
-	if winMatrix[them] == us {
-		return 0 + us, nil
-	}
+	case "Z":
+		// We win
+		return WinningScore + lostMatrix[them], nil
 
-	return WinningScore + us, nil
+	default:
+		return 0, fmt.Errorf("unknown answer: %s", elements[1])
+	}
 }
 
-func mapToConst(element string) (int, error) {
+func mapToElement(element string) (int, error) {
 	switch element {
-	case "A", "X":
+	case "A":
 		return Rock, nil
-	case "B", "Y":
+	case "B":
 		return Paper, nil
-	case "C", "Z":
+	case "C":
 		return Scissors, nil
 	default:
 		return 0, fmt.Errorf("unknown element: %s", element)
